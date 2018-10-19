f6c1c45e92e59c3f7ee604b4e8367fa685e631de
Reza N <32329310+rezan21@users.noreply.github.com>
2018-10-20 00:43:15 +0100

    Added Animation functions
    added functions and using Daneden's Animate.css

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-$(document).ready(function(){
+$(document).ready(function()  {
 
   $('.js--scroll-to-contact').click(function(){
-    $('html, body').animate({scrollTop: $('.js--section-contact').offset().top},1000);
-
+    $('html, body').animate({scrollTop: $('.js--section-contact').offset().top},1500);
   });
 
   $('.js--scroll-to-services').click(function(){
-    $('html, body').animate({scrollTop: $('.js--section-services').offset().top},1000);
+    $('html, body').animate({scrollTop: $('.js--section-services').offset().top},1500);
   });
 
   $('.js--scroll-to-clinic').click(function(){
-    $('html, body').animate({scrollTop: $('.js--section-clinic').offset().top},1000);
+    $('html, body').animate({scrollTop: $('.js--section-clinic').offset().top},1500);
   });
 
   $('.js--scroll-to-album').click(function(){
-    $('html, body').animate({scrollTop: $('.js--section-alnum').offset().top},1000);
+    $('html, body').animate({scrollTop: $('.js--section-album').offset().top},1500);
   });
+
+
+
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeInLeft');
+  }, {
+    offset: '100%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated bounceInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
+  });
+
+
 
 });
