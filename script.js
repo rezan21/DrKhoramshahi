e5d3e705befa625c3b46aa7a6951aaf196dd1822
Reza N <32329310+rezan21@users.noreply.github.com>
2018-10-30 23:38:59 +0000

    sticky_navigation_js

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-$(document).ready(function()  {
+$(document).ready(function() {
 
-  $('.js--scroll-to-contact').click(function(){
-    $('html, body').animate({scrollTop: $('.js--section-contact').offset().top},1500);
+  //scroll to sections
+  $('.js--scroll-to-contact').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js--section-contact').offset().top
+    }, 1500);
   });
 
-  $('.js--scroll-to-services').click(function(){
-    $('html, body').animate({scrollTop: $('.js--section-services').offset().top},1500);
+  $('.js--scroll-to-services').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js--section-services').offset().top
+    }, 1500);
   });
 
-  $('.js--scroll-to-clinic').click(function(){
-    $('html, body').animate({scrollTop: $('.js--section-clinic').offset().top},1500);
+  $('.js--scroll-to-clinic').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js--section-clinic').offset().top
+    }, 1500);
   });
 
-  $('.js--scroll-to-album').click(function(){
-    $('html, body').animate({scrollTop: $('.js--section-album').offset().top},1500);
+  $('.js--scroll-to-album').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js--section-album').offset().top
+    }, 1500);
   });
 
 
 
+  //animated sections
   $('.js--wp-1').waypoint(function(direction) {
     $('.js--wp-1').addClass('animated fadeInLeft');
   }, {
-    offset: '100%'
+    offset: '80%'
   });
 
   $('.js--wp-2').waypoint(function(direction) {
@@ -42,6 +52,32 @@ $(document).ready(function()  {
     offset: '50%'
   });
 
+  //display and dismiss sticky bar
+  $('.js--sticky').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+      var nav = $('.js--main-nav');
+      nav.hide(500);
+
+    }
+  }, {
+    offset: '50px'
+  });
+
+  //toggle button
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    nav.slideToggle(500);
+  });
+  //dismiss toggle
+
+  $('.js--nav-close').click(function() {
+    var nav = $('.js--main-nav');
+    nav.hide(500);
+
+  });
 
 
 });
